39055cf3252a65d6e1d482f97f0a2852c294c83b
Nik Kelly <Nikolay.Kelly@corebts.com>
2020-01-06 13:33:26 -0800

    ignore untagged items and save json results

--- scrape.py
+++ scrape.py
@@ -1,9 +1,10 @@
 """ 
-1. Connect to reddit
-2. Scrape /r/buildapcsales posts
+Possible considerations:
+    Host on Azure Functions to run automatically
+1. Connect to reddit (done)
+2. Scrape /r/buildapcsales posts (done)
     a. repeat daily
 3. Save all scraped posts to Azure Blob as JSON
-    a. https://pypi.org/project/azure-storage-blob/
  """
 import praw
 import config
@@ -16,10 +17,17 @@ subreddit = reddit.subreddit('buildapcsales')
 submissions = []
 
 lastScrape = datetime.datetime.utcnow() - datetime.timedelta(hours=24)
+submissionCount = 0
+for submission in subreddit.new(limit=None):
+    # Skip posts that have expired or don't have flair
+    if(submission.link_flair_text == None or submission.link_flair_text == 'Expired :table_flip:'):
+        continue
+    if(datetime.datetime.utcfromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S') > lastScrape.strftime('%Y-%m-%d %H:%M:%S')):
+        submissions.append([submission.title, submission.link_flair_text, submission.id, submission.permalink, submission.url, datetime.datetime.utcfromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S')])
+        submissionCount += 1
 
-for submission in subreddit.new(limit=3):
-    submissions.append([submission.title, submission.id, submission.permalink, submission.url, datetime.datetime.fromtimestamp(submission.created_utc)])
+print(str(submissionCount) + ' total submissions scraped')
 
-submissions = pd.DataFrame(submissions, columns=['title','id','permalink','link','created'])
-jsonData = submissions.to_json()
-print(jsonData)
+submissions = pd.DataFrame(submissions, columns=['title','flair','id','permalink','link','created'])
+todaysDate = datetime.date.today()
+jsonData = submissions.to_json(r'testFile.json')
--- testAzure.py
+++
@@ -1,3 +0,0 @@
-from azure.storage.blob import BlobServiceClient
-
-service = BlobServiceClient(account_url="https://buildapcscrape.blob.core.windows.net/", credential=credential)
